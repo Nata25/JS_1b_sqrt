20f545a7b28ad23b575f979fb528bd307b5f25d5
Natalya Ivanova <natalyas.ivanova@gmail.com>
2016-11-08 00:53:28 +0200

    Manage period in input number

--- js/script.js
+++ js/script.js
@@ -1,17 +1,37 @@
-var number = 253009;
+// var number = 36;
 // var number = 625;
 // var number = 138384;
+var number = 2530.09;
+// var number = 360;
+
+// Calculate square root of n
+// @param n number >=0
+// @return result number
 
 function sqrt_new(n) {
     var step = 1;
     var guess = 0;
-    var currentResult = guess;
-    var diff, max, test;
+    var result = guess;
+    var diff, max, prevTest, nextTest;
     var parts = [];
+    var period, shift;
+
+    var strToNum = n.toString();
+    var len = strToNum.length - 1;
+
+    // find period
+    period = strToNum.toString().indexOf('.');
+    if (period != -1) {
+        // strToNum = strToNum.substring(0, period) + strToNum.substring(period+1);
+        shift = len - period;
+        if (shift % 2 == 0) {
+            n = n * Math.pow(10, shift);
+            strToNum = n.toString();
+            shift = Math.pow(0.1, shift / 2);
+        }
+    }
 
     // break number to parts
-    var strToNum = n.toString();
-    var len = strToNum.length;
     if (len % 2 != 0) {
         strToNum = " " + strToNum;
     }
@@ -22,33 +42,34 @@ function sqrt_new(n) {
     // find first number of result
     max = parts[0];
     do {
+        prevTest = nextTest;
         guess += step;
-        test = guess * guess;
-    }
-    while (test <= max);
+        nextTest = guess * guess;
+    } while (nextTest <= max);
 
     guess -= step;
-    currentResult = guess;
-    test = guess * guess;
+    result = guess;
 
     // find the rest of numbers
     var iterations = parts.length - 1;
 
     for (var i = 0; i < iterations; i++) {
-
-        diff = max - test;
+        diff = max - prevTest;
         max = parseInt(diff + parts[i+1]);
-        guess = 0;
+        guess = nextTest = 0;
         do {
+            prevTest = nextTest;
             guess++;
-            test = +((currentResult * 2).toString() + guess) * guess;
-        } while  (test <= max);
+            nextTest = +((result * 2).toString() + guess) * guess;
+        } while (nextTest <= max);
 
         guess -= step;
-        test = +((currentResult * 2).toString() + guess) * guess;
-        currentResult = +(currentResult.toString() + guess.toString());
+        result = +(result.toString() + guess.toString());
    }
-    return currentResult;
+   if (shift > 0) {
+       result = result * shift;
+   }
+   return result;
 }
 
 console.log(sqrt_new(number));
